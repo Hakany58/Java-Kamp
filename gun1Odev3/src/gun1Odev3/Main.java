6e09cf9dfb97d11645de22fb63fc8e78d695f3fb
Hakan YILDIRIM <5hknyldrm8@gmail.com>
2021-05-07 15:24:26 +0300

    FirsCommit

---
+++ gun1Odev3/src/gun1Odev3/Main.java
@@ -0,0 +1,110 @@
+package gun1Odev3;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("eheheh");
+		
+		
+		//String mesaj = "Hakan";
+		
+	/*	System.out.println(mesaj);
+		
+		System.out.println("Son harf = "+ mesaj.charAt(4));
+		
+		System.out.println("Kaç karakter = "+ mesaj.length());
+		
+		System.out.println("Ýsim H ile baþlýyorsa true yazdýr "+"= "+ mesaj.startsWith("H")); //startWith("x")= x ile baþlýyor mu? demek
+		
+		System.out.println("Ýsim n ile bitiyorsa false yazdýr "+"= "+ mesaj.endsWith("n")); //endWith("x") = x ile bitiyor mu? demek
+		
+		System.out.println(mesaj.indexOf("a"));//a harfi ÝLK SEFER ne zaman kullanýldý (deðiþkende)
+		
+		System.out.println(mesaj.lastIndexOf("a"));//a harfi EN SON ne zaman kullanýldý (deðiþkende)
+	
+		System.out.println(mesaj.replace("a", "e"));// a olan yerlere e yazdýr, oldChar --> newChar
+		
+		System.out.println();
+	
+	*/
+		
+
+
+
+
+		//ASAL SAYI 
+/*		System.out.println("Asal olup olmadýðýný öðrenmek istediðiniz sayýyý giriniz..");
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int number = scanner.nextInt();
+		int i;
+		if(number>2) {
+		for(i=2;i<number;i++) {
+			if(number % i == 0) {
+				System.out.println("Asal Sayý Deðildir");
+				i=number;
+			}else if(number % i != 0) {
+				System.out.println("Asal Sayýdýr");
+				i=number;
+			}
+		}
+		}else if(number == 2) {
+			System.out.println("Asal Sayýdýr");
+		}else {
+			System.out.println("Pozitif bir sayý giriniz!");
+		}
+
+
+		*/
+
+
+
+	/*		//ÝNCE-KALIN SESLÝ HARFLER
+	  
+		 Scanner scan = new Scanner(System.in);
+		
+		 System.out.print("Bir harf giriniz: ");
+	        char harf = scan.next().charAt(0);
+	        switch(harf) {
+			case 'A':
+			case 'O':
+			case 'U':
+			case 'I':
+			case 'a':
+			case 'o':
+			case 'u':
+			case 'ý':
+				System.out.println("Kalýn sesli harf");
+				break;
+			case 'E':
+			case 'Ö':
+			case 'Ü':
+			case 'Ý':
+			case 'e':
+			case 'ö':
+			case 'ü':
+			case 'i':
+				System.out.println("Ýnce sesli harf");
+				break;
+			default:
+				System.out.println("Lütfen sesli harf giriniz");
+		}
+		
+		
+		*/
+		
+				
+
+		
+		
+		
+		
+		
+		
+	}
+		
+	
+
+}
